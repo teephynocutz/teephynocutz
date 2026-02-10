3a04fad46ac5747a798b3b6af09b0cddc1aaf59b
Teephyno Cutz <teephynocutz@gmail.com>
2026-02-10 13:46:07 +0200

    fix frontend endpoint / redirect problem

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -156,7 +156,7 @@ export default function BookingPage() {
 
     console.log("BOOKING PAYLOAD →", payload)
 
-    const res = await api.post("api/bookings", payload)
+    const res = await api.post("https://teephynocutz-backend.vercel.app/api/bookings", payload)
 
     console.log("BOOKING SUCCESS →", res.data)
     setSuccess(true)
